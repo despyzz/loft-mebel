758b3097464baf02e74f295f5e66324d5d033ccc
yzz <yzzdespair@gmail.com>
2024-02-04 23:16:04 +0300

    ErrorBoundary added

--- src/index.tsx
+++ src/index.tsx
@@ -2,14 +2,17 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './app';
 import {BrowserRouter} from "react-router-dom";
+import ErrorBoundary from "./app/providers/ErrorBoundary";
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
   <React.StrictMode>
-    <BrowserRouter>
-      <App/>
-    </BrowserRouter>
+      <BrowserRouter>
+        <ErrorBoundary>
+          <App/>
+        </ErrorBoundary>
+      </BrowserRouter>
   </React.StrictMode>
 );
