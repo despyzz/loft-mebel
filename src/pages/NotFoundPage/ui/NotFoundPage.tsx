686613f80c3fdae81e948842e139060e4d297fdd
yzz <yzzdespair@gmail.com>
2024-02-04 23:13:23 +0300

    changed ErrorPage to NotFoundPage

---
+++ src/pages/NotFoundPage/ui/NotFoundPage.tsx
@@ -0,0 +1,11 @@
+import classes from './NotFoundPage.module.scss';
+
+const NotFoundPage = () => {
+  return (
+    <div>
+      <h1 className={classes.title}>Not Found Page</h1>
+    </div>
+  );
+}
+
+export default NotFoundPage;
