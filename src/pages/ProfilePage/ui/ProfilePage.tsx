25b3777ae772c325780f7c4283015c460968fc0b
yzz <yzzdespair@gmail.com>
2024-02-14 09:27:43 +0300

    Profile page added

---
+++ src/pages/ProfilePage/ui/ProfilePage.tsx
@@ -0,0 +1,13 @@
+// import styles from './ProfilePage.module.scss'
+
+import {BonusProgram} from "widgets/BonusProgram";
+
+const ProfilePage = () => {
+  return (
+    <div>
+      <BonusProgram />
+    </div>
+  );
+};
+
+export default ProfilePage;
--- src/shared/config/AppRouterConfig/AppRouterConfig.tsx
+++ src/shared/config/AppRouterConfig/AppRouterConfig.tsx
@@ -2,19 +2,22 @@ import {RouteProps} from "react-router-dom";
 import {MainPage} from "pages/MainPage";
 import {AboutPage} from "pages/AboutPage";
 import {NotFoundPage} from "pages/NotFoundPage";
-import {CartPage} from "../../../pages/CartPage";
+import {CartPage} from "pages/CartPage";
+import {ProfilePage} from "pages/ProfilePage";
 
 export enum AppRoutes {
   MAIN = 'main',
   ABOUT = 'about',
   ERROR = 'error',
   CART = 'cart',
+  PROFILE = 'profile',
 }
 
 export const RoutePath: Record<AppRoutes, string> = {
   [AppRoutes.MAIN]: '/',
   [AppRoutes.ABOUT]: '/about',
   [AppRoutes.CART]: '/cart',
+  [AppRoutes.PROFILE]: '/profile',
   [AppRoutes.ERROR]: '/*',
 }
 
@@ -30,6 +33,10 @@ export const AppRouterConfig: Record<AppRoutes, RouteProps> = {
   [AppRoutes.CART]: {
     path: RoutePath.cart,
     element: <CartPage />
+  },
+  [AppRoutes.PROFILE]: {
+    path: RoutePath.profile,
+    element: <ProfilePage />
   },
   [AppRoutes.ERROR]: {
     path: RoutePath.error,
