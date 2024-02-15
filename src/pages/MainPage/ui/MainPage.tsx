f3cb40d202cf67cd62139d3c9a2ffc91dcc7dad0
yzz <yzzdespair@gmail.com>
2024-02-15 12:21:27 +0300

    Adaptive navigation

--- src/pages/MainPage/ui/MainPage.tsx
+++ src/pages/MainPage/ui/MainPage.tsx
@@ -2,10 +2,12 @@ import classes from './MainPage.module.scss';
 import {Promo} from "widgets/Promo"
 import AppContainer from "shared/ui/AppContainer/AppContainer";
 import {ProductCard} from "widgets/ProductCard";
+import {Navigation} from "widgets/Navigation";
 
 const MainPage = () => {
   return (
     <>
+      <Navigation />
       <Promo />
       <AppContainer>
         <div className={classes.Cards}>
