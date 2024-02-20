387ef19fa6ae54fced8f64638e43fa61c18e0722
yzz <yzzdespair@gmail.com>
2024-02-20 23:37:02 +0300

    just for test (add LoginModal)

--- src/pages/MainPage/ui/MainPage.tsx
+++ src/pages/MainPage/ui/MainPage.tsx
@@ -3,11 +3,34 @@ import {Promo} from "widgets/Promo"
 import AppContainer from "shared/ui/AppContainer/AppContainer";
 import {Navigation} from "widgets/Navigation";
 import {Products} from "widgets/Products";
+import AppButton from "shared/ui/AppButton/AppButton";
+import {useState} from "react";
+import {LoginModal} from "../../../feautures/AuthByUsername";
 
 const MainPage = () => {
 
+  const [isAuthModal, setIsAuthModal] = useState(false);
+  const closeIsAuthModal = () => setIsAuthModal(false);
+  const openIsAuthModal = () => setIsAuthModal(true);
+
+
   return (
     <div className={classes.MainPage}>
+
+      <AppContainer>
+        <AppButton
+          style={{border: "1px solid grey", padding: "15px", color: "grey", fontSize: "16px"}}
+          onClick={openIsAuthModal}
+        >
+          show login modal
+        </AppButton>
+      </AppContainer>
+
+      <LoginModal
+        isOpen={isAuthModal}
+        onClose={closeIsAuthModal}
+      />
+
       <Navigation/>
       <Promo/>
       <AppContainer>
