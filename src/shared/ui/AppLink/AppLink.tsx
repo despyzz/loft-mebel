dcb5138e95202796526f69b7f7b2b563ba218396
yzz <yzzdespair@gmail.com>
2024-02-29 23:26:00 +0300

    add link themes

--- src/shared/ui/AppLink/AppLink.tsx
+++ src/shared/ui/AppLink/AppLink.tsx
@@ -1,19 +1,46 @@
-import React, {FC} from 'react';
+import React, {FC, memo} from 'react';
 import {Link, LinkProps} from "react-router-dom";
 import classes from "./AppLink.module.scss";
 import classNames from "classnames";
 
+enum AppLinkTheme {
+  UNDERLINE,
+  CLEAR
+}
+
 interface AppLinkProps extends LinkProps {
-  className?: string
+  className?: string,
+  theme?: AppLinkTheme,
 }
 
 const AppLink: FC<AppLinkProps> = (props: AppLinkProps) => {
-  const {to, className, children, ...other} = props;
+  const {
+    to,
+    className,
+    children,
+    theme,
+    ...other
+  } = props;
+
+  let themeClass;
+  switch (theme) {
+    case AppLinkTheme.CLEAR:
+      themeClass = classes.Clear;
+      break;
+
+    case AppLinkTheme.UNDERLINE:
+      themeClass = classes.Underline;
+      break;
+
+    default:
+      themeClass = classes.Clear;
+  }
+
   return (
-    <Link to={to} className={classNames(className, classes.Link)} {...other}>
+    <Link to={to} className={classNames(className, themeClass, classes.Link)} {...other}>
       {children}
     </Link>
   );
 };
 
-export default AppLink;
+export default memo(AppLink);
