d28d2e4c456cbf5d7dc32b4b6333806d8236acc7
yzz <yzzdespair@gmail.com>
2024-03-20 20:55:56 +0300

    test products-info endpoint

--- src/pages/WishlistPage/ui/WishlistPage.tsx
+++ src/pages/WishlistPage/ui/WishlistPage.tsx
@@ -1,47 +1,68 @@
-import React, {memo, useEffect} from 'react';
+import React, {memo} from 'react';
 import classes from './WishlistPage.module.scss';
 import {Page} from "widgets/Page";
 import AppContainer from "shared/ui/AppContainer/AppContainer";
-import {DynamicModuleLoader} from "../../../shared/lib/components/DynamicModuleLoader";
-import {ReducerList} from "../../../shared/lib/components/DynamicModuleLoader/DynamicModuleLoader";
-import {wishlistReducer} from "../../../entities/Wishlist";
-import {useAppDispatch} from "../../../shared/lib/hooks/useAppDispatch/useAppDispatch";
-import {fetchWishlist} from "../../../entities/Wishlist/model/services/fetchWishlist";
+import {getWishlistData, getWishlistError, getWishlistIsLoading} from "entities/Wishlist";
 import {useSelector} from "react-redux";
-import {getUserAuthData, userReducer} from "../../../entities/User";
-// import {
-//   getWishlistData,
-//   getWishlistError,
-//   getWishlistIsLoading
-// } from "../../../entities/Wishlist/model/selectors/wishlistSelectors";
-
-const reducers: ReducerList = {
-  wishlist: wishlistReducer,
-  user: userReducer
-}
+import Loader from "../../../widgets/Loader";
+import AppButton, {AppButtonTheme} from "../../../shared/ui/AppButton/AppButton";
 
 const WishlistPage = () => {
-  const authData = useSelector(getUserAuthData)
-  const userId = authData?.id;
+  const data = useSelector(getWishlistData)
+  const error = useSelector(getWishlistError)
+  const isLoading = useSelector(getWishlistIsLoading)
 
-  const dispatch = useAppDispatch();
+  if (error) {
+    return (
+      <Page className={classes.WishlistPage}>
+        <AppContainer>
+          <h1>
+            Произошла ошибка при загрузке данных
+          </h1>
+        </AppContainer>
+      </Page>
+    )
+  }
 
-  // const data = useSelector(getWishlistData)
-  // const error = useSelector(getWishlistError)
-  // const isLoading = useSelector(getWishlistIsLoading)
-
-  useEffect(() => {
-    if (userId)
-      dispatch(fetchWishlist(userId))
-  }, [dispatch, userId]);
+  if (isLoading) {
+    return (
+      <Page className={classes.WishlistPage}>
+        <AppContainer>
+          <Loader/>
+        </AppContainer>
+      </Page>
+    )
+  }
 
   return (
     <Page className={classes.WishlistPage}>
-      <DynamicModuleLoader reducers={reducers}>
-        <AppContainer>
-          wishlist)
-        </AppContainer>
-      </DynamicModuleLoader>
+      <AppContainer>
+        <AppButton
+          theme={AppButtonTheme.DARK}
+          onClick={() => {
+            if (!data)
+              return
+
+            fetch('http://localhost:8000/products-info', {
+              method: 'POST',
+              headers: {
+                'Content-Type': 'application/json',
+              },
+              body: JSON.stringify({
+                ids: data.productsIds
+              }),
+            })
+              .then(response => response.json())
+              .then(data => console.log(data))
+              .catch((error) => {
+                console.error('Error:', error);
+              });
+
+          }}
+        >
+          aboba
+        </AppButton>
+      </AppContainer>
     </Page>
   );
 };
