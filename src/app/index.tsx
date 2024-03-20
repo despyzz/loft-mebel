af0dcf355b270952b7c743427e74dbf63cd0eec4
yzz <yzzdespair@gmail.com>
2024-03-20 20:54:43 +0300

    load wishlist and cart on start app

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -3,12 +3,23 @@ import './styles/index.scss';
 import AppRouter from "./providers/AppRouter/ui/AppRouter";
 import Header from "widgets/Header";
 import Footer from "widgets/Footer";
-import {useDispatch} from "react-redux";
-import {userActions} from "../entities/User";
+import {useSelector} from "react-redux";
+import {getUserAuthData, userActions} from "../entities/User";
+import {useAppDispatch} from "../shared/lib/hooks/useAppDispatch/useAppDispatch";
+import { fetchCart } from 'entities/Cart';
+import { fetchWishlist } from 'entities/Wishlist';
 
 function App() {
+  const dispatch = useAppDispatch();
+  const authData = useSelector(getUserAuthData)
 
-  const dispatch = useDispatch();
+  useEffect(() => {
+    const userId = authData?.id;
+    if (userId) {
+      dispatch(fetchWishlist(userId));
+      dispatch(fetchCart(userId));
+    }
+  }, [dispatch, authData]);
 
   useEffect(() => {
     dispatch(userActions.initAuthData());
